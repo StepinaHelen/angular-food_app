f175a6b0625022821e88b7254bdb77ea7abbaa78
StepinaHelen <stopinahelen@gmail.com>
2023-01-02 12:22:53 +0100

    add firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+};
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDgcq6QbdRTqwo9roZWy5rDTXkWFyze8Ts',
+  authDomain: 'foodapp-5f221.firebaseapp.com',
+  projectId: 'foodapp-5f221',
+  storageBucket: 'foodapp-5f221.appspot.com',
+  messagingSenderId: '921449123814',
+  appId: '1:921449123814:web:9ef8e4d2feb365c2e75027',
+  measurementId: 'G-H1Y112VNJG',
 };
 
 /*
