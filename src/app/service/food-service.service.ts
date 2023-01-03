7cb9bf27e54c80f70e3a9ebed7a6d6002be0f0ce
StepinaHelen <stopinahelen@gmail.com>
2023-01-03 13:58:52 +0100

    add component folder

--- src/app/service/food-service.service.ts
+++ src/app/service/food-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodServiceService {
+  constructor(private afs: AngularFirestore) {}
 
-  constructor() { }
+  getFoodsList(): Observable<any> {
+    const listCollection = this.afs.collection('foods');
+    return listCollection.stateChanges().pipe(
+      map((dataRes) => {
+        return dataRes.map((list: any) => ({
+          ...list.payload.doc.data(),
+          id: list.payload.doc.id,
+          type: list.type,
+        }));
+      })
+    );
+  }
 }
