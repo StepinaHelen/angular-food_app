af962f25054f25c8d2e97527151e7a252ed8d2cd
Serhii Kalashnykov <cyborgx999@gmail.com>
2023-01-04 12:27:27 +0100

    Create shared card item component

---
+++ src/app/shared/modules/card/card-item/card-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FoodInterface } from 'src/app/service/types';
+
+@Component({
+  selector: 'food-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss'],
+})
+export class CardItemComponent implements OnInit {
+  @Input()
+  food: FoodInterface | null = null;
+  public foodAmount: number = 1;
+
+  decrement() {
+    if (this.foodAmount === 1) return;
+    this.foodAmount -= 1;
+  }
+
+  increment() {
+    this.foodAmount += 1;
+  }
+
+  //TODO: Implement adding to cart
+  handleAdd() {
+    this.foodAmount = 1;
+  }
+
+  ngOnInit(): void {}
+}
