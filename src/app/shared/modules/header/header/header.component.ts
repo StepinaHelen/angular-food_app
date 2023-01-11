b5986a3e30137825ef8de1b19266a40ac81ccd46
StepinaHelen <stopinahelen@gmail.com>
2023-01-11 21:18:18 +0100

    add fixes

--- src/app/shared/modules/header/header/header.component.ts
+++ src/app/shared/modules/header/header/header.component.ts
@@ -4,6 +4,7 @@ import { CartService } from 'src/app/service/cart.service';
 import { LocalStorageService } from 'src/app/service/local-storage.service';
 import { ThemeService } from 'src/app/service/theme.service';
 import { LocalStorageKeys } from 'src/app/service/types';
+import { ITHEMES } from 'src/app/shared/types/types';
 
 @Component({
   selector: 'food-header',
@@ -12,7 +13,7 @@ import { LocalStorageKeys } from 'src/app/service/types';
 })
 export class HeaderComponent implements OnInit {
   cartItemAmount$: Observable<number>;
-  theme: string =
+  theme: keyof ITHEMES =
     this.localStorageService.getLocalStorageItem(LocalStorageKeys.theme) ||
     'default';
 
@@ -33,7 +34,7 @@ export class HeaderComponent implements OnInit {
       );
   }
 
-  changeTheme(themeName: string) {
+  changeTheme(themeName: keyof ITHEMES) {
     this.theme = themeName;
     this.themeService.setTheme(themeName);
     this.localStorageService.setLocalstorageItem(
