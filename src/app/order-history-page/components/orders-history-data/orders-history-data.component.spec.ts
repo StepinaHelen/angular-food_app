58414b643dc22191823f2eecb1bbbe59a368df06
StepinaHelen <stopinahelen@gmail.com>
2023-01-18 21:05:05 +0100

    add test for Hero component

--- src/app/order-history-page/components/orders-history-data/orders-history-data.component.spec.ts
+++ src/app/order-history-page/components/orders-history-data/orders-history-data.component.spec.ts
@@ -1,12 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularFireModule } from '@angular/fire/compat';
 import { OrdersHistoryDataComponent } from './orders-history-data.component';
 import { OrderHistoryService } from 'src/app/service/order-history-service.service';
+import { ItemHistoryDataMock } from '../../../shared/testing-moks/testing-mocks';
+import { of } from 'rxjs';
 
 describe('OrdersHistoryDataComponent', () => {
   let component: OrdersHistoryDataComponent;
   let fixture: ComponentFixture<OrdersHistoryDataComponent>;
   let orderHistoryServiceSpy: jasmine.SpyObj<OrderHistoryService>;
+  let historyElement: HTMLElement;
 
   beforeEach(async () => {
     const spy = jasmine.createSpyObj('OrderHistoryService', [
@@ -24,10 +26,44 @@ describe('OrdersHistoryDataComponent', () => {
     orderHistoryServiceSpy = TestBed.inject(
       OrderHistoryService
     ) as jasmine.SpyObj<OrderHistoryService>;
+
+    historyElement = fixture.nativeElement;
+    component.ordersHistory$ = spy.getHistoryOrderItem.and.returnValue(
+      of(ItemHistoryDataMock)
+    );
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('[Order History Page]should display Phone number:', () => {
+    const phone = historyElement.querySelectorAll('.phone')[0];
+    expect(phone?.textContent).toContain(ItemHistoryDataMock[0].phone);
+  });
+
+  it('[Order History Page] should display First and last name', () => {
+    const name = historyElement.querySelectorAll('.name')[0];
+    expect(name?.textContent).toEqual(
+      `${ItemHistoryDataMock[0].firstName}` +
+        ' ' +
+        `${ItemHistoryDataMock[0].lastName}`
+    );
+  });
+
+  it('[Order History Page]should display Adress:', () => {
+    const adress = historyElement.querySelectorAll('.adress')[0];
+    expect(adress?.textContent).toContain(
+      `${ItemHistoryDataMock[0].city}` +
+        ', ' +
+        `${ItemHistoryDataMock[0].street}`
+    );
+  });
+
+  it('[Order History Page]should display Date', () => {
+    const date = historyElement.querySelector('.date p');
+    expect(date?.textContent).toContain(ItemHistoryDataMock[0].date);
   });
 });
--- src/app/shared/testing-moks/testing-mocks.ts
+++ src/app/shared/testing-moks/testing-mocks.ts
@@ -7,3 +7,34 @@ export const FoodItemWithAmountMock = {
   id: '2',
   type: 'added',
 };
+
+export const ItemHistoryDataMock = [
+  {
+    city: '44444444',
+    date: 'Jan 10, 2023',
+    firstName: 'ssss',
+    lastName: 'sssssssss',
+    phone: '444444444444',
+    street: '44444444',
+    foods: [
+      {
+        amount: 1,
+        category: 'drinks',
+        id: 'LFS3GlIPuaPcny0zRxiW',
+        img: 'https://stylesatlife.com/wp-content/uploads/2020/01/black-coffee-benefits.jpg',
+        price: 10,
+        title: 'Black Coffee',
+        type: 'added',
+      },
+      {
+        amount: 1,
+        category: 'pizza',
+        id: 'U3fUK1HxUuvIEz4V7SCe',
+        img: 'https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg',
+        price: 12,
+        title: 'Cheese pizza',
+        type: 'added',
+      },
+    ],
+  },
+];
