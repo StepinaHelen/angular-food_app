820b666c172a76c7b706b4f87c2c18df8d38011c
StepinaHelen <stopinahelen@gmail.com>
2023-01-19 14:12:37 +0100

    add test for service

--- src/app/service/order-history-service.service.spec.ts
+++ src/app/service/order-history-service.service.spec.ts
@@ -1,29 +1,58 @@
 import { TestBed } from '@angular/core/testing';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { SpinnerService } from './spinner.service';
+import { of } from 'rxjs';
 
 import { OrderHistoryService } from './order-history-service.service';
+import {
+  ItemForHistoryOrderMock,
+  ItemHistoryDataMock,
+} from '../shared/testing-moks/testing-mocks';
+
+const collectionStub = {
+  add: jasmine
+    .createSpy('add')
+    .and.returnValue([...ItemHistoryDataMock, ItemForHistoryOrderMock]),
+  valueChanges: jasmine
+    .createSpy('valueChanges')
+    .and.returnValue(of(ItemHistoryDataMock)),
+  pipe: jasmine.createSpy('pipe').and.returnValue(ItemHistoryDataMock),
+};
+
+const angularFiresotreStub = {
+  collection: jasmine.createSpy('collection').and.returnValue(collectionStub),
+};
 
 describe('OrderHistoryService', () => {
   let service: OrderHistoryService;
-  let angularFirestoreSpy: jasmine.SpyObj<AngularFirestore>;
+  let angularFirestore: AngularFirestore;
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('AngularFirestore', ['collection']);
-
     TestBed.configureTestingModule({
       providers: [
         OrderHistoryService,
-        { provide: AngularFirestore, useValue: spy },
+        { provide: AngularFirestore, useValue: angularFiresotreStub },
       ],
     });
     service = TestBed.inject(OrderHistoryService);
-    angularFirestoreSpy = TestBed.inject(
-      AngularFirestore
-    ) as jasmine.SpyObj<AngularFirestore>;
+    angularFirestore = TestBed.get(AngularFirestore);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call function', () => {
+    service.addOrderItemToHistory(ItemForHistoryOrderMock);
+    expect(angularFiresotreStub.collection).toHaveBeenCalledWith(
+      'ordersHistory'
+    );
+  });
+
+  it('should call function2', () => {
+    service.getHistoryOrderItem();
+    expect(angularFiresotreStub.collection).toHaveBeenCalledWith(
+      'ordersHistory'
+    );
   });
 });
