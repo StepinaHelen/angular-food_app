0a925c7edba559441c32ad589253719238ed8553
Serhii Kalashnykov <cyborgx999@gmail.com>
2023-01-27 13:36:42 +0100

    Update header component to use sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
-
-import { AppComponent } from './app.component';
-
-import { firebaseConfig } from 'src/environments/environment';
-
-import { FooterModule } from './shared/modules/footer/footer/footer.module';
-import { OrderFormPageModule } from './order-form-page/order-form-page.module';
-import { MainPageModule } from './main-page/main-page.module';
-import { HeaderModule } from './shared/modules/header/header/header.module';
-import { OrderHistoryPageModule } from './order-history-page/order-history-page.module';
-import { CartPageModule } from './cart-page/cart-page.module';
-import { CartService } from './service/cart.service';
+import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { firebaseConfig } from 'src/environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CartPageModule } from './cart-page/cart-page.module';
+import { MainPageModule } from './main-page/main-page.module';
+import { OrderFormPageModule } from './order-form-page/order-form-page.module';
+import { OrderHistoryPageModule } from './order-history-page/order-history-page.module';
+import { CartService } from './service/cart.service';
+import { FooterModule } from './shared/modules/footer/footer/footer.module';
+import { HeaderModule } from './shared/modules/header/header/header.module';
+import { SidenavModule } from './shared/modules/sidenav/sidenav.module';
 import { SpinnerModule } from './shared/modules/spinner/spinner.module';
 
 @NgModule({
@@ -31,6 +29,7 @@ import { SpinnerModule } from './shared/modules/spinner/spinner.module';
     OrderHistoryPageModule,
     CartPageModule,
     SpinnerModule,
+    SidenavModule,
   ],
 
   providers: [CartService],
